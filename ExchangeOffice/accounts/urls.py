dbf37c59165965ab645e3e7595798c087e0c8852
Gulakov Vadim <gulakovvadim2@gmail.com>
2021-12-04 01:40:55 +0300

    Implement reg/login/logout forms and views

--- ExchangeOffice/accounts/urls.py
+++ ExchangeOffice/accounts/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from accounts.views import SignUpView
+from .views import office, register, edit
+from django.contrib.auth.views import logout_then_login, LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup')
+    path('register/', register, name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='login'),
+    path('logout-then-login/', logout_then_login, name='logout_then_login'),
+    path('', office, name='office'),
+    path('edit/', edit, name='edit'),
+    path('password-change/', PasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
--- ExchangeOffice/accounts/views.py
+++ ExchangeOffice/accounts/views.py
@@ -1,9 +1,61 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views import generic
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
+from .models import Profile
 
 
-class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
+# def user_login(request):
+#     if request.method == 'POST':
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             cd = form.cleaned_data
+#             user = authenticate(username=cd['username'], password=cd['password'])
+#             if user is not None:
+#                 if user.is_active:
+#                     login(request, user)
+#                     return HttpResponse('Authenticated successfully')
+#                 else:
+#                     return HttpResponse('Disabled account')
+#             else:
+#                 return HttpResponse('Invalid login')
+#     else:
+#         form = LoginForm()
+#     return render(request, 'accounts/registration/login.html', {'form': form})
+
+
+@login_required
+def office(request):
+    return render(request, 'accounts/office.html', {'section': 'office'})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            profile = Profile.objects.create(user=new_user)
+            return render(request, 'registration/register_done.html', {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request, 'registration/signup.html', {'user_form': user_form})
+
+
+@login_required
+def edit(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = ProfileEditForm(instance=request.user.profile, data=request.POST, files=request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = ProfileEditForm(instance=request.user.profile)
+        return render(request,
+                      'accounts/edit.html',
+                      {'user_form': user_form,
+                       'profile_form': profile_form})
