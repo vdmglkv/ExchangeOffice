3f7b17b9c0a78daa9f3fbe4fc95ac11c16e3381a
Gulakov Vadim <gulakovvadim2@gmail.com>
2021-12-04 15:20:58 +0300

    Update models

--- ExchangeOffice/accounts/models.py
+++ ExchangeOffice/accounts/models.py
@@ -7,6 +7,7 @@ class Profile(models.Model):
     date_of_birth = models.DateField(blank=True, null=True)
     photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
     balance = models.JSONField(default={'USD': 1000, 'EUR': 1000, 'BYN': 1000})
+    limit = models.IntegerField(default=1000)
 
     def __str__(self):
         return 'Profile for user {}'.format(self.user.username)
