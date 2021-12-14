3a824d5551b3067f5c5afb2dd99730cffe87a2ff
Gulakov Vadim <gulakovvadim2@gmail.com>
2021-12-14 19:33:58 +0300

    Implement operation model

--- ExchangeOffice/OfficeApp/models.py
+++ ExchangeOffice/OfficeApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from accounts.models import Profile
 
-# Create your models here.
+
+class Operation(models.Model):
+    user = models.OneToOneField(Profile, on_delete=models.CASCADE)
+    currency_in = models.CharField(max_length=10)
+    currency_out = models.CharField(max_length=10)
+    value_in = models.DecimalField(max_digits=19, decimal_places=10)
+    value_out = models.DecimalField(max_digits=19, decimal_places=10)
+    date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user
