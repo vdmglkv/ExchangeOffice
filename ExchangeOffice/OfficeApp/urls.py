753c92ec857500655a4d868da2429199902f9889
Gulakov Vadim <gulakovvadim2@gmail.com>
2021-12-14 19:35:48 +0300

    Add endpoint to convert_done

--- ExchangeOffice/OfficeApp/urls.py
+++ ExchangeOffice/OfficeApp/urls.py
@@ -3,6 +3,7 @@ from . import views
 urlpatterns = [
     path('', views.main),
     path('change/', views.change),
+    path('change_done/', views.change_done),
     path('contact/', views.contact),
     path('currencyrate/', views.currency_rate),
     path('client/', views.client),
