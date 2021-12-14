328ff46c45e8d965ab4d7f321bc9ae133c2798b6
Gulakov Vadim <gulakovvadim2@gmail.com>
2021-12-14 19:35:16 +0300

    Update views

--- ExchangeOffice/OfficeApp/views.py
+++ ExchangeOffice/OfficeApp/views.py
@@ -1,4 +1,8 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, HttpResponse
+from django.contrib.auth import get_user
+from .models import Operation
+from .forms import Convert
 
 # Create your views here.
 
@@ -12,7 +16,20 @@ def contact(request):
 
 
 def change(request):
-    return render(request, "OfficeApp/change.html")
+    if request.method == 'POST':
+        form = Convert(request.POST)
+        if form.is_valid():
+            user = get_user(request)
+            operation = Operation.objects.create(user_id=user.id, value_in=1, value_out=1)
+            operation.save()
+            return HttpResponseRedirect('/change_done/')
+    else:
+        form = Convert()
+    return render(request, "OfficeApp/change.html", {'form': form})
+
+
+def change_done(request):
+    return render(request, "OfficeApp/convert_done.html")
 
 
 def currency_rate(request):
