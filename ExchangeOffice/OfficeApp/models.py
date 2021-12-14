7b6eef11564851151ace8ba4a434423e06e20b65
Gulakov Vadim <gulakovvadim2@gmail.com>
2021-12-14 23:16:04 +0300

    Update models

--- ExchangeOffice/OfficeApp/models.py
+++ ExchangeOffice/OfficeApp/models.py
@@ -3,11 +3,13 @@ from accounts.models import Profile
 
 
 class Operation(models.Model):
-    user = models.OneToOneField(Profile, on_delete=models.CASCADE)
-    currency_in = models.CharField(max_length=10)
-    currency_out = models.CharField(max_length=10)
-    value_in = models.DecimalField(max_digits=19, decimal_places=10)
-    value_out = models.DecimalField(max_digits=19, decimal_places=10)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    currency_in = models.CharField(choices=(('USD', 'USD'), ('EUR', 'EUR'), ('BYN', 'BYN')),
+                                   name='From', max_length=3, default='USD')
+    value = models.DecimalField(max_digits=19, decimal_places=10, default=1)
+    currency_out = models.CharField(choices=(('USD', 'USD'), ('EUR', 'EUR'), ('BYN', 'BYN')),
+                                    name='To',  max_length=3, default='USD')
+    result = models.DecimalField(max_digits=19, decimal_places=10, default=1)
     date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
